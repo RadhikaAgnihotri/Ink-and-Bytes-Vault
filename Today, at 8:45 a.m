0b2a6c050f01053f3b53766ee8036018405f99f5
RadhikaAgnihotri <65966180+RadhikaAgnihotri@users.noreply.github.com>
2024-05-24 20:25:36 +0530

    Today, at 8:45 a.m

--- Today, at 8:45 a.m
+++ Today, at 8:45 a.m
@@ -1,7 +1,12 @@
-#The idea is to bring a shift in our vibrations at 8:45 in the morning because, this time seems to me
+The idea is to bring a shift in our vibrations at 8:45 in the morning because, this time seems to me
 as a time when it is neither too late or too early to make that change fo rthe day,
 Like I don't want you to be worried right when you wake up about having to drop all the negativity and 
 Step into a high vibration, positive zone[but in reality you think you haven't given time to your
 negative thoughts, so you are worried again] But this clearly doesn't mean that you can't do it early
 You always can! As early as Now!
 
+
+I am sad now, I think I'm hurting myself,
+But tomorrow I'll change, and read something from that shelf.
+I notice it's titled, "Why the World is a Sham,"
+I make a mental note, to read it all, tomorrow at 8:45 a.m.
