667d2f311c734d587607082782969e67aa5904f7
RadhikaAgnihotri <65966180+RadhikaAgnihotri@users.noreply.github.com>
2024-05-25 20:12:19 +0530

    Today, at 8:45 a.m

--- Today, at 8:45 a.m
+++ Today, at 8:45 a.m
@@ -3,7 +3,7 @@ as a time when it is neither too late or too early to make that change fo rthe d
 Like I don't want you to be worried right when you wake up about having to drop all the negativity and 
 Step into a high vibration, positive zone[but in reality you think you haven't given time to your
 negative thoughts, so you are worried again] But this clearly doesn't mean that you can't do it early
-You always can! As early as Now!
+You always can! As early as Now! 
 
 
 I am sad now, I think I'm hurting myself,
